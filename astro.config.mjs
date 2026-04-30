6900499c353e3a3a08d638b24b58fdc41c3e57d5
Diego Pena <diegopf.dev@gmail.com>
2026-04-30 19:02:56 +0200

    fix: configuración de base path y preview script para GitHub pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://diegopefer.github.io",
   base: "/portfolio/",
-  outDir: "./dist",
   vite: {
     plugins: [tailwindcss()],
+    // Añade esto:
+    build: {
+      assetsInlineLimit: 0,
+    }
   },
-  build: {
-    assets: "assets"
-  },
-  
 });
