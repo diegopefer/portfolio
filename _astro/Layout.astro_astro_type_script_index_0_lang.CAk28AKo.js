3cacf84d16ac69a2d1fb8e97fd4aff566122455b
diegopefer <diegopefer@users.noreply.github.com>
2026-04-30 17:16:55 +0000

    deploy: feca209f757a9b80261a7e417568938a0afbc035

---
+++ _astro/Layout.astro_astro_type_script_index_0_lang.CAk28AKo.js
@@ -0,0 +1,1 @@
+localStorage.getItem("color-theme")==="dark"||!("color-theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark");
